2a182f1b66f8306d1a452505dbb4cf45d3a3137d
MiszaPawlicki <miszapawlicki7@gmail.com>
2023-03-30 16:33:38 +0100

    journey order to be implemented

--- NLP.py
+++ NLP.py
@@ -1,4 +1,6 @@
 import random
+import re
+
 import nltk
 import spacy
 import json
@@ -16,40 +18,6 @@ station_names = [i['stationName'].lower() for i in stations]
 f = open('KB.json')
 intents = json.load(f)
 #
-def lemmatize_and_clean(text):
-    doc = nlp(text.lower())
-    out = ""
-    for token in doc:
-        if not token.is_stop and not token.is_punct:
-            out = out + token.lemma_ + " "
-    return out.strip()
-
-
-def get_closest_station_match(station):
-    '''
-        This function takes a string, 'station', and finds the closest match. If the match has a similarity
-        score of 0.9 or above, only the closest station is returned. Otherwise, the three closest stations are returned
-    '''
-    closest_matches = difflib.get_close_matches(station,station_names)
-    score = difflib.SequenceMatcher(None, station, closest_matches[0]).ratio()
-    return_stations = []
-    print(closest_matches)
-    print(score)
-    if score>0.9:
-        for i in stations:
-            if i['stationName'].lower() == closest_matches[0]:
-                print(i)
-                return_stations.append(i)
-                break
-        return return_stations
-    else:
-        for m in closest_matches:
-            for i in stations:
-                if i['stationName'].lower() == m:
-                    print(i)
-                    return_stations.append(i)
-                    break
-        return return_stations
 
 def check_intent(user_sentence):
 
@@ -65,13 +33,15 @@ def check_intent(user_sentence):
     doc1 = nlp(user_sentence)
     for intention_type in intents:
         for kb_sent in intents[intention_type]['patterns']:
+            #kb_sent = re.sub(r'\#\w+', '', kb_sent)
+            clean_sent = re.sub(r'\#\w+', '', kb_sent)
             doc2 = nlp(kb_sent)
             similarity = doc1.similarity(doc2)
             if similarity>max_similarity:
                 max_similarity = similarity
                 most_similar_sentence = kb_sent
                 intent = intention_type
-    print(max_similarity)
+    print(most_similar_sentence)
     #if not sure of intent
     if(max_similarity<0.65):
         return None
@@ -96,7 +66,11 @@ def find_station_in_sentence(sentence):
     return [station for station in stations if station['stationName'].lower() in matching_stations]
 
 
-
+def infer_journey_order(user_input):
+    '''
+        Function that takes a user input and infers where the from, to, when and return
+    '''
+    return None
 
 def get_response(intent):
     '''

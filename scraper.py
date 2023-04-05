2c2d71eb5a36c2ab3d84670bb48e4b9df5756816
Rxberts <85238263+Rxberts@users.noreply.github.com>
2023-04-05 16:22:29 +0100

    scraper changes - ready for finding link

--- scraper.py
+++ scraper.py
@@ -1,47 +1,65 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-import chromedriver_binary
-import requests
-def scrape_ticket(origin, destination, departure_time, date):
-    #time - format 1200
-    #date - format - 140523 or today
-    #origin - crs code
-    #destination - crs code
+from datetime import datetime
+import requests  # needs to be installed !!!
+from bs4 import BeautifulSoup  # needs to be installed !!!!
+
+def cheapest_ticket(origin, destination, date, departure_time, ticket_type):
+    """
+        Function that finds the cheapest ticket from start to destination using web scraping.
+
+        :param origin: The starting location the user is going to get a train from. Location in abbreviation form for train
+        e.g. Norwich is NRW
+        :param destination: This is the location that the user is trying to get to
+        :param leave_date: The leaving date for train
+        :param leave_time: The leaving time for train. Time needs to be in time of interval of 15 minutes: eg: 11:00,11:15,
+        11:30 or 11:45
+        :return: tuple that contains:
+                string of amount for cheapest ticket
+                string of leave time
+                string of arrival time
+                string containing hyperlink to get ticket
+        """
+    # time - format 1200
+    # date - format - 140523 or today
+    # origin - crs code
+    # destination - crs code
     url = "https://ojp.nationalrail.co.uk/service/timesandfares/[ORIGIN]/[DESTINATION]/[DATE]/[TIME]/dep"
-    #EXAMPLE URL = https://ojp.nationalrail.co.uk/service/timesandfares/NRW/BTN/140523/1630/dep
+    # EXAMPLE URL = https://ojp.nationalrail.co.uk/service/timesandfares/NRW/BTN/140523/1630/dep
     url = url.replace("[ORIGIN]", origin)
     url = url.replace("[DESTINATION]", destination)
     url = url.replace("[TIME]", departure_time)
     url = url.replace("[DATE]", date)
+    get_national_rail = requests.get(url)
+    soup = BeautifulSoup(get_national_rail.content, "html.parser")
 
-    response = requests.get(url)
-    html = response.content
-    soup = BeautifulSoup(html, 'html.parser')
+    cheapest_section = soup.find("td", {"class": "fare has-cheapest"})  # find the section where it has the cheapest fare
+    label = cheapest_section.find("label")  # find the first label on the webpage
+    cost = label.text.strip()  # remove whitespace
 
-
-    ticket_details = []
-    table_elements = soup.find('table', {'id': 'oft'})
-    for row in table_elements.find_all('tr'):
-
-        #print(row)
-        dep = row.find('div', {'class': 'dep'})
-        arr = row.find('div', {'class': 'arr'})
-        price = row.find('label', {'class': 'opsingle'})
+    ticket_section = cheapest_section.parent
+    actual_dep_time = ticket_section.find("div", {"class": "dep"}).text.strip()
+    actual_arrival_time = ticket_section.find("div", {"class": "arr"}).text.strip()
+    return cost,actual_dep_time,actual_arrival_time,"hyperlink to website"
 
 
-        if dep and arr and price:
-            ticket_details.append({"departure": dep.text.strip(), "arrival": arr.text.strip(), "price": price.text.strip(), "url": url})
 
-        lowest_ticket = None
-        lowest_price = None
-        for ticket in ticket_details:
-            if lowest_price == None:
-                lowest_ticket = ticket
-            elif lowest_price<ticket['price']:
-                lowest_ticket = ticket
+def main():
+    #convert string into time object:
+    time_string = "11:00"
+    time_value = datetime.strptime(time_string, '%H:%M').time()
+    print(time_value.hour)
+
+    date_string = "23/05/2023"
+    date_value = datetime.strptime(date_string, '%d/%m/%Y').date()
+    print(date_value.month)
+
+    #  make sure these are all valid inputs! - all in the future, and are real dates as well as real places to go to and
+    #  from !
 
 
-    return lowest_ticket
+
+    cheapest_ticket("NRW", "KGX", date_value, time_value,"dep")
 
 
-#scrape_ticket("NRW","BTN","1200","140523")
+if __name__ == "__main__":
+    main()
+

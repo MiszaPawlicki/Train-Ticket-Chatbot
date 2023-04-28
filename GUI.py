2f0211510d1c072c7c250baf8d117862c33cc180
MiszaPawlicki <miszapawlicki7@gmail.com>
2023-04-28 12:40:37 +0100

    nlp refactored for use in GUI. Return functionality needs work

--- GUI.py
+++ GUI.py
@@ -1,37 +1,41 @@
 import tkinter as tk
+import random
 
 import NLP
 
-
-def send_message():
-    '''
-        This function takes a user input and generates a response using the generate respose function
-        in NLP.py, generate_response returns a string
-    '''
-    message = user_input.get()
-    response = NLP.generate_response(message)
-    chat_log.insert(tk.END, 'You: ' + message + '\n')
-    chat_log.insert(tk.END, 'Chatbot: ' + response + '\n')
-    user_input.delete(0, tk.END)
-
-
-#code to setup the basic window
+# Create the main window
 root = tk.Tk()
-root.title('Train Chatbot')
+root.title("Chatbot GUI")
 
-#size params
-chat_log = tk.Text(root, height=40, width=100)
-chat_log.pack()
+# Create the chat history window
+history_window = tk.Text(root, height=20, width=50)
+history_window.pack()
 
-#text input params
-user_input = tk.Entry(root, width=50)
-user_input.pack()
+# Create the chat input field
+input_field = tk.Entry(root, width=50)
+input_field.pack()
 
-#send button params
-send_button = tk.Button(root, text='Send', command=send_message)
-send_button.pack()
+# Function to handle sending a message
+# Function to handle sending a message
+def send_message():
+    message = input_field.get()
+    history_window.insert(tk.END, "You: " + message + "\n")
+    input_field.delete(0, tk.END)
+    response = NLP.generate_response(message)
+    history_window.insert(tk.END, "Bot: " + response + "\n")
 
-#inserting initial hello message
-chat_log.insert(tk.END, 'Chatbot: Hello, how may I help?' + '\n')
 
+def func(event):
+    send_message()
+
+def onclick(event):
+    send_message()
+
+root.bind('<Return>', onclick)
+
+button = tk.Button(root, text="Send")
+button.bind('<Button-1>', onclick)
+button.pack()
+
+# Start the main loop
 root.mainloop()

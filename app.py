814630091c5f0a6ecc76bc8bdc78484234e6e4ae
JamesMcG13 <98750609+JamesMcG13@users.noreply.github.com>
2023-05-29 17:43:38 +0100

    Added text-to-speech for system

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template, request
 import NLP
+import pyttsx3
 
 app = Flask(__name__)
 app.static_folder = 'static'
+
+def speak(text):
+    text_speech = pyttsx3.init()
+    text_speech.say(text)
+    text_speech.runAndWait()
+
+
 @app.route("/")
 def home():
+    #Speaks before loading and delays the system, not sure if we should just skip the intro message or not.
+    #speak("Hi, I am your personal ticket assistant! How can I help you today?")
     return render_template("index.html")
+
+
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return NLP.generate_response(userText)
+    response = NLP.generate_response(userText)
+    speak(response)
+    return response
+
 if __name__ == "__main__":
     app.run()

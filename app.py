755f21803547ae9912db0c2ef1733a57001bc3a1
JamesMcG13 <98750609+JamesMcG13@users.noreply.github.com>
2023-05-30 17:03:31 +0100

    added to KB

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import NLP
 import pyttsx3
 import threading
 
+# Start by initialising flask and linkning the static folder
 app = Flask(__name__)
 app.static_folder = 'static'
 
+# Function for operating the text-to-speech functionality
 def speak(text):
     text_speech = pyttsx3.init()
     text_speech.say(text)
     text_speech.runAndWait()
 
 
+# Initialise the index page use threading so that messages can appear and be read at the same time
 @app.route("/")
 def home():
     speach_thread = threading.Thread(target=speak, args=("Hi, I am your personal ticket assistant! How can I help you today?",))
@@ -19,6 +22,7 @@ def home():
     return render_template("index.html")
 
 
+# Retrieve responses from the bot and check for hyperlink, if found do not read that part
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
